eb1e3e8356d40ad159801ff6eba4834b4f7f6442
emitoma <toma.emi.95.07.17@gmail.com>
2020-10-11 21:28:30 +0200

    Create migrations.

--- backend/__tests__/index.test.js
+++ backend/__tests__/index.test.js
@@ -1,4 +1,4 @@
-const dummyFunc = require('../index');
+const dummyFunc = require('../src/index');
 
 test('adds 2+3 to equal to 5', () => {
   expect(dummyFunc(2, 3)).toBe(5);
---
+++ backend/src/config.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+const dotenvExists = fs.existsSync('.env.local');
+
+if (dotenvExists) {
+  const envConfig = dotenv.parse(fs.readFileSync('.env.local'));
+
+  for (const k in envConfig) {
+    process.env[k] = envConfig[k];
+  }
+}
+
+module.exports = {
+  mysql: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+};
