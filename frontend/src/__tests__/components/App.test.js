c2f8f134dcd4295c1ebd306746118fbe739dd451
emitoma <toma.emi.95.07.17@gmail.com>
2020-10-11 19:45:18 +0200

    Clean up frontend folder, fix tests.

--- frontend/src/__tests__/components/App.test.js
+++ frontend/src/__tests__/components/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../../components/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = getByText(/This is my app/i);
+  expect(h1Element).toBeInTheDocument();
 });
