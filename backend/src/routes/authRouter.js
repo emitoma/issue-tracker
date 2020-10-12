8663903a64d32ffa9926659b110c5dce580e4220
emitoma <toma.emi.95.07.17@gmail.com>
2020-10-12 22:29:06 +0200

    Start using Joi, for validating input data at register.

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -6,10 +6,17 @@ const router = express.Router();
 router.use(express.json());
 
 router.post('/register', (req, res) => {
-  const data = authService.register(req.body.emailAddress, req.body.password);
+  const data = authService.register(
+    req.body.emailAddress,
+    req.body.password,
+    req.body.repeatPassword
+  );
+  console.log(data);
 
   if (data.status === 200) {
     res.status(200).json(data);
+  } else if (data.status === 401) {
+    res.status(401).json(data);
   } else if (data.status === 500) {
     res.status(500).json(data);
   }
--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,7 +1,44 @@
 const userQueries = require('../queries/userQueries');
+const joi = require('joi');
+const Joi = require('joi');
+const { invalid } = require('joi');
 
-const register = async (emailAddress, password) => {
+const validateRegister = (inputEmail, inputPassword, inputPwRepeat) => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: true } })
+      .min(1),
+
+    password: Joi.string()
+      .pattern(/^[a-zA-Z0-9]{3,30}$/)
+      .required(),
+
+    repeat_password: Joi.ref('password'),
+  }).with('email', 'password', 'repeat_password');
+
+  const data = {
+    email: inputEmail,
+    password: inputPassword,
+    repeat_password: inputPwRepeat,
+  };
+
+  Joi.validate(data, schema, (err, value) => {
+    if (err) {
+      return {
+        status: 401,
+        message: 'invalid input data',
+      };
+    } else {
+      return value;
+    }
+  });
+};
+
+const register = async (emailAddress, password, repeatPassword) => {
   try {
+    const validValue = validateRegister(emailAddress, password, repeatPassword);
+    console.log(validValue);
+
     let newUser = await userQueries.createUser(emailAddress, password);
     return newUser;
   } catch (error) {
