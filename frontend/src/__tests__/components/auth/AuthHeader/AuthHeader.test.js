67c7b830a9bbe01f029777895e0b04fb988103ab
emitoma <toma.emi.95.07.17@gmail.com>
2020-11-07 16:49:43 +0100

    Add auth tests.

--- frontend/src/__tests__/components/auth/AuthHeader/AuthHeader.test.js
+++ frontend/src/__tests__/components/auth/AuthHeader/AuthHeader.test.js
@@ -4,6 +4,7 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import Login from '../../../../pages/auth/login';
 import AuthHeader from '../../../../components/auth/AuthHeader';
+import LoginForm from '../../../../components/auth/form/LoginForm';
 
 configure({ adapter: new Adapter() });
 
@@ -11,5 +12,10 @@ describe('<Login/>', () => {
   it('should render the auth header', () => {
     const wrapper = shallow(<Login />);
     expect(wrapper.find(AuthHeader)).toHaveLength(1);
+  });
+
+  it('should render the login form if isLogin true', () => {
+    const wrapper = shallow(<AuthHeader />);
+    expect(wrapper.find(LoginForm));
   });
 });
